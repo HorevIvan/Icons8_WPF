96f92ff16590ceb6fc1966d709ae4c76c65951dc
Horev Ivan <ivanhorev@icons8.com>
2017-03-13 02:43:02 +0700

    added base classes for icon controls

---
+++ Icons8.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Icons8.UI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(I8_Assembly.Company)]
+[assembly: AssemblyProduct(I8_Assembly.Product)]
+[assembly: AssemblyCopyright(I8_Assembly.Copyright)]
+[assembly: AssemblyTrademark(I8_Assembly.Trademark)]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion(I8_Assembly.Version)]
+[assembly: AssemblyFileVersion(I8_Assembly.Version)]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1ad1e676-0a5b-46d0-8ba2-6de3c83a5eac")]
+
+
+public static class I8_Assembly
+{
+    public const string Product = "Icons8";
+    public const string Copyright = "Copyright © Icons8 2017";
+    public const string Trademark = "Icons8";
+    public const string Company = "Icons8 LLC";
+
+
+    public const string Version = "0.1.0.0";
+}
