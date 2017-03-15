0cfcb3bbdac86234ceca69e478184c02e623d321
Horev Ivan <ivanhorev@icons8.com>
2017-03-15 15:33:59 +0700

    FillColor

--- Icons8.UI/IconControl.cs
+++ Icons8.UI/IconControl.cs
@@ -4,10 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Icons8.UI
 {
     public abstract class IconControl : UserControl
     {
+        public virtual Image GetIconImage()
+        {
+            return (Image)VisualTreeHelper.GetChild(VisualTreeHelper.GetChild(VisualTreeHelper.GetChild(this, 0), 0), 0);
+        }
+
+        private Image _Image;
+
+        public Image Image => _Image ?? (_Image = GetIconImage());
+
+        private Drawing[] _Drawings;
+
+        public Drawing[] Drawings => _Drawings ?? (_Drawings = GetDrawings(DrawingImage));
+
+        public DrawingImage DrawingImage => (DrawingImage)Image.Source;
+
+        public static Drawing[] GetDrawings(DrawingImage drawingImage)
+        {
+            if(drawingImage == null) return new Drawing[0];
+
+            if(drawingImage.Drawing is DrawingGroup)
+            {
+                return GetDrawings(drawingImage.Drawing as DrawingGroup);
+            }
+            else if(drawingImage.Drawing is GeometryDrawing)
+            {
+                return GetDrawings(drawingImage.Drawing as DrawingGroup);
+            }
+            else
+            {
+            }
+
+            return null;
+        }
+
+        public static Drawing[] GetDrawings(DrawingGroup drawingGroup)
+        {
+            if(drawingGroup == null) return new Drawing[0];
+
+            var colors = new List<Color>();
+
+            var drawings = new List<Drawing>();
+
+            foreach(var drawing in drawingGroup.Children)
+            {
+                if(drawing is DrawingGroup)
+                {
+                     drawings.AddRange(GetDrawings(drawing as DrawingGroup));
+                }
+                else if(drawing is GeometryDrawing)
+                {
+                    drawings.Add(drawing as GeometryDrawing);
+                }
+                else
+                {
+
+                }
+            }
+
+            return drawings.ToArray();
+        }
     }
 }
--- Icons8.UI/MonochromeIconControl.cs
+++ Icons8.UI/MonochromeIconControl.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Icons8.UI
 {
     public abstract class MonochromeIconControl : IconControl
     {
+        public virtual void FillColor(Color color)
+        {
+            foreach(var drawing in Drawings)
+            {
+                FillColor(drawing, color);
+            }
+        }
+
+        private void FillColor(Drawing drawing, Color color)
+        {
+            if(drawing is GeometryDrawing)
+            {
+                FillColor(drawing as GeometryDrawing, color);
+            }
+            else
+            {
+            }
+        }
+
+        private void FillColor(GeometryDrawing drawing, Color color)
+        {
+            if(drawing.Brush != null)
+            {
+                drawing.Brush = FillColor(drawing.Brush, color);
+            }
+
+            if(drawing.Pen != null)
+            {
+                drawing.Pen = FillColor(drawing.Pen, color);
+            }
+        }
+
+        private Pen FillColor(Pen pen, Color color)
+        {
+            var brush = FillColor(pen.Brush, color);
+
+            return new Pen(brush, pen.Thickness);
+        }
+
+        private Brush FillColor(Brush brush, Color color)
+        {
+            if(brush is SolidColorBrush)
+            {
+                return new SolidColorBrush(color);
+            }
+            else
+            {
+                return brush;
+            }
+        }
     }
 }
